60a3d374f82bcc83115cca5b1df210f500465f2d
tsviatkou <i.tsviatkou@alcosi.eu>
2023-08-09 19:30:00 +0300

    init project

---
+++ ARKitForEx/ARKitForEx/ARKitForExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitForExApp.swift
+//  ARKitForEx
+//
+//  Created by IvanDev on 9.08.23.
+//
+
+import SwiftUI
+
+@main
+struct ARKitForExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
